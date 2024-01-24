b60d67cdc13ce0ff5c3d35cc7ed86d7fd564df99
rishadislam1 <rishad.islam158@gmail.com>
2024-01-24 22:58:12 +0600

    initial setup done

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+// mongodb setup
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.izzbewl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+// my all database related code. First connect database and create collections.
+
+    const database = client.db('shopManagement');
+    const productCollection = database.collection('products');
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
+
+// full app get
+app.get('/', (req,res)=>{
+    res.send('Shop Is Running');
+})
+
+app.listen(port, ()=>{
+    console.log(`Shop is running on port ${port}`);
+})
